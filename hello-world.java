64383c936d08ec1cbde74785b851374d3881da73
Rohit Rana <72288734+itsrohitsinghrana@users.noreply.github.com>
2026-04-22 10:11:11 +0530

    Update hello-world.java

--- hello-world.java
+++ hello-world.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
+public class hello_world {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Rohit , How are you!");
     }
 }
